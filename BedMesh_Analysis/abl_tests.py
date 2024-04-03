2449b338243c32db6c0177c8a0b198c2b383400e
Josh Hollis <joshuahollis608@gmail.com>
2024-04-02 21:40:39 -0500

    Add data analysis functionality to abl_tests.py

--- BedMesh_Analysis/abl_tests.py
+++ BedMesh_Analysis/abl_tests.py
@@ -1,4 +1,5 @@
 from duet_http import get_reply_sequence, recieve_reply, get_duet_status, download_file, send_gcode_command
+import numpy as np
 import sys
 import datetime
 import time
@@ -86,6 +87,51 @@ def perform_abl(duet_ip, abl_cmd, save_path, itr):
 	# download the heightmap, and decrement the iterations
 	save_heightmap(duet_ip, save_path, coefficients, itr)
 
+def create_file(save_path, file_name, arr, header):
+	file_path = os.path.join(save_path, file_name)
+	with open(file_path, 'w') as file:
+		file.write(header + '\n')
+		for row in arr:
+			for val in row:
+				file.write(str(format(val, '.3f')) + ', ')
+			file.write('\n')
+
+def data_analysis(save_path):
+	#open heightmap files
+	heightmaps = []
+	for i in range(ITERATIONS):
+		file_path = os.path.join(save_path, f'heightmap_{i}.csv')
+		heightmaps.append(file_path)
+	#determine the dimensions of the heightmap by reading the last two entries in line 3
+	with open(heightmaps[0], 'r') as file:
+		lines = file.readlines()
+		dimensions = lines[2].split(',')
+		y_dim = int(dimensions[-1])
+	#array of heightmaps is formatted as [heightmap][y][x]
+	#ex: array[0][1][2] would be the z value of the first heightmap at row 2, column 3
+	heightmap_values = []
+	for heightmap in heightmaps:
+		with open(heightmap, 'r') as file:
+			tmp_arr = []
+			lines = file.readlines()
+			lines = lines[3:3+y_dim]
+			for i in range(len(lines)):
+				z_values = lines[i].split(', ')
+				#remove spaces and the newline character
+				for j in range(len(z_values)):
+					z_values[j] = z_values[j].strip()
+				z_values = [float(x) for x in z_values]
+				tmp_arr.append(z_values)
+			heightmap_values.append(tmp_arr)
+	#calculate the standard deviation of each point
+	np_array = np.array(heightmap_values)
+	std_dev = np.round(np.std(np_array, axis=0),3)
+	std_dev = std_dev.tolist()
+	create_file(save_path, 'std_dev.csv', std_dev, 'Standard Deviation')
+	#calculate the range of each point
+	range_arr = np.round(np.ptp(np_array, axis=0),3)
+	range_arr = range_arr.tolist()
+	create_file(save_path, 'range.csv', range_arr, 'Range')
 
 if __name__ == "__main__":
 	if check_connection(DUET_IP):
@@ -94,5 +140,6 @@ if __name__ == "__main__":
 			for i in range(ITERATIONS):
 				perform_abl(DUET_IP, SZP_ABL_CMD, save_path, i)
 				# perform_abl(DUET_IP, BLTOUCH_ABL_CMD, save_path, i)
+			data_analysis(save_path)
 	else:
 		sys.exit(1)
